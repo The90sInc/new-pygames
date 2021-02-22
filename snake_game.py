21186e706a54f73772965deed5ee0a8ffb9aabcf
techguru <niyotokunlola@gmail.com>
2021-02-23 00:54:55 +0100

    created a game loop, keeping screen from disappearing

--- snake_game.py
+++ snake_game.py
@@ -3,5 +3,12 @@ import pygame
 pygame.init()
 dis = pygame.display.set_mode((400,300))
 pygame.display.update()
+
+#creating a game loop to keep screen from disappearing
+pygame.display.set_caption("Snake Game using Pygame")
+game_over = False
+while not game_over:
+    for event in pygame.event.get():
+        print(event) #prints out all the actions that take place on the  screen
 pygame.quit()
 quit()
